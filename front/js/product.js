a073a8b83407c1735e50c7e6fbbd5d1a6366a93c
Hyacine Alnuma <hyacinealnuma@gmail.com>
2022-05-23 13:55:39 +0200

    product.js done

--- front/js/product.js
+++ front/js/product.js
@@ -1,14 +1,90 @@
 const parseURL = new URL (window.location.href);
 const id = parseURL.searchParams.get("id");
+const itemImg = document.getElementsByClassName("item__img");
+
 
 if (sessionStorage.getItem("products") != null) {
     let data = sessionStorage.getItem("products");
     data = JSON.parse(data);
+    display(data);
+} else {
+    fetch("http://localhost:3000/api/products")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            sessionStorage.setItem("products", JSON.stringify(data));
+            display(data);
+        })
+        .catch(err => console.log("error", err))
+}
 
+function display(data) {
     for (let product of data) {
         if (product._id == id) {
-            console.log(product.name);
+            const sofaImage = document.createElement("img");
+            sofaImage.setAttribute('src', `${product.imageUrl}`);
+            sofaImage.setAttribute('alt', `${product.altTxt}`);
+            itemImg[0].appendChild(sofaImage);
+
+            const sofaName = document.getElementById("title");
+            sofaName.innerHTML = product.name;
+
+            const sofaPrice = document.getElementById("price");
+            sofaPrice.innerHTML = product.price;
+
+            const sofaDescription = document.getElementById("description");
+            sofaDescription.innerHTML = product.description;
+
+            for (let color of product.colors) {
+                const colorOption = document.createElement("option");
+                const selectColor = document.getElementById("colors");
+                colorOption.innerHTML = color;
+                colorOption.setAttribute('value', `${color}`);
+                selectColor.appendChild(colorOption);
+            }
             break;
         }
     }
-} 
+}
+
+let cart = [];
+const addBtn = document.getElementById("addToCart");
+
+
+function addToCart() {
+    let quantity = document.getElementById("quantity").value;
+    //Pour sélectionner la couleur
+    let color = document.getElementById("colors").selectedIndex;
+    color = document.getElementsByTagName("option")[color].value;
+
+    //"S'il n'y a aucune commande dans la session"
+    if (localStorage.getItem("cart") == null) {
+        let order = {id: id, color: color, quantity:quantity};
+        cart.push(order);
+        localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+    //S'il y a déjà une commande dans la session    
+        let localCart = JSON.parse(localStorage.getItem("cart"));
+        let productNotInCart = true;
+        for (let product of localCart) {
+        if (product.id === id && product.color === color) {
+            product.quantity = Number(product.quantity) + Number(quantity);
+            localStorage.setItem("cart", JSON.stringify(localCart));
+            productNotInCart = false;
+            break;
+        }};
+        if (productNotInCart) {
+            let order = {id: id, color: color, quantity:quantity};
+            localCart.push(order);
+            localStorage.setItem("cart", JSON.stringify(localCart));
+        }
+    }
+}
+
+addBtn.addEventListener("click", function() {
+    addToCart();
+});
+
+
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -14,29 +14,27 @@ if (sessionStorage.getItem("products") == null) {
     render(data);
 }
 
-function render(data) {
-   
+function render(data) { 
     for (let i of data) {
-   
-    const cardSofaArticle = document.createElement("article");
-    const cardSofa = document.createElement("a");
-    cardSofa.setAttribute('href', `./product.html?id=${i._id}`);
+        const cardSofaArticle = document.createElement("article");
+        const cardSofa = document.createElement("a");
+        cardSofa.setAttribute('href', `./product.html?id=${i._id}`);
 
-    const cardSofaName = document.createElement("h3");
-    cardSofaName.innerHTML = i.name;
+        const cardSofaName = document.createElement("h3");
+        cardSofaName.innerHTML = i.name;
 
-    const cardSofaImg = document.createElement("img");
-    cardSofaImg.setAttribute('src', `${i.imageUrl}`);
-    cardSofaImg.setAttribute('alt', `${i.altTxt}`);
+        const cardSofaImg = document.createElement("img");
+        cardSofaImg.setAttribute('src', `${i.imageUrl}`);
+        cardSofaImg.setAttribute('alt', `${i.altTxt}`);
 
-    const cardSofaDescription = document.createElement("p");
-    cardSofaDescription.innerHTML = `${i.description}`;
+        const cardSofaDescription = document.createElement("p");
+        cardSofaDescription.innerHTML = `${i.description}`;
 
-    items.appendChild(cardSofa);
-    cardSofa.appendChild(cardSofaArticle);
-    cardSofaArticle.appendChild(cardSofaName);
-    cardSofaArticle.appendChild(cardSofaImg);
-    cardSofaArticle.appendChild(cardSofaDescription);
+        items.appendChild(cardSofa);
+        cardSofa.appendChild(cardSofaArticle);
+        cardSofaArticle.appendChild(cardSofaName);
+        cardSofaArticle.appendChild(cardSofaImg);
+        cardSofaArticle.appendChild(cardSofaDescription);
     }
 }
 
